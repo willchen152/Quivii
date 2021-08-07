e6e8b66753d52b1bda6294d1d1aea5a159b0fe2c
PotatoDistribution <60849417+PotatoDistribution@users.noreply.github.com>
2021-08-07 10:33:32 -0400

    Update Data.py

--- Data.py
+++ Data.py
@@ -1,22 +1,21 @@
-Teams = {
-    "A": {
-        "members": ["person1", "person2"],
-        "wins": 3,
-        "record": [1, 1, 1]
-    },
-    "B": {
-        "members": ["person1", "person2"],
-        "wins": 2,
-        "record": [1, 1, 0]
-    },
-    "C": {
-        "members": ["person1", "person2"],
-        "wins": 1,
-        "record": [0, 0, 1]
-    },
-    "D": {
-        "members": ["person1", "person2"],
-        "wins": 0,
-        "record": [0, 0, 0]
-    }
+import shelve
+
+db = shelve.open('test.dat')
+db['A']={
+    'members': ['will', 'zhenhao', 'jason', 'xiang'],
+    'wins': [0],
+    'record': [0, 0, 0]
 }
+db['B']={
+    'members': ['person1', 'person2', 'person 3'],
+    'wins': [3],
+    'record': [1, 1, 1]
+}
+
+for team in db:
+    print('Team: '+ team)
+    for key in db[team]:
+        print('\n' + key + ':')
+        for item in db[team][key]:
+            print(item, end = ', ')
+    print()
