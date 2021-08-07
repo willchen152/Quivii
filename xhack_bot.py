c6dd825a7ba675537dbc03ab39bc98d9af42e8b7
Zli123123 <67983161+Zli123123@users.noreply.github.com>
2021-08-07 14:21:07 -0400

    Update xhack_bot.py

--- xhack_bot.py
+++ xhack_bot.py
@@ -1,4 +1,3 @@
-
 import discord
 from discord.ext import commands
 import shelve
@@ -11,6 +10,8 @@ async def on_ready():
     print('Bot is ready')
     channel = client.get_channel(873356724722081832)
     await channel.send("Bot is online")
+    db = shelve.open('tournament.dat')
+    db['teams'] = []    
 
 @client.command()
 async def test(ctx):
@@ -20,7 +21,8 @@ async def test(ctx):
     db = shelve.open('tournament.dat')
     db['person'] = name
     print(db['person'])
-      
+
+
 @client.command()
 async def addteam(ctx, teamname, members):
     db = shelve.open('tournament.dat')
@@ -30,11 +32,52 @@ async def addteam(ctx, teamname, members):
     db[teamname] = {
         'members': members,
         'wins': [0],
-        'record': [0]
+        'record': []
     }
+    
+        
+    teams = db["teams"] #array of teamnames
+    teams.append(teamname)
+    db['teams'] = teams    
+    
     await ctx.send("Added!")
-    db = shelve.close()
 
+    
+    
+    
+
+@client.command()
+async def nextround(ctx):
+    db = shelve.open('tournament.dat')
+    teams = db['teams']     
+    round_num = []
+    for team in teams:
+        round_num.append(len(db[team]['record']))
+    counter = 0
+    mininum = min(round_num)
+    for item in round_num:
+        if item != mininum:
+            counter += 1
+    if counter > 0:
+        await ctx.send(f'there are {counter} teams not done round.')
+    else:
+        
+        data = []
+        for team in range(len(teams)):
+            team_name = teams[team]
+            here_data = []
+            here_data.append(db[team_name]['wins'][0]) 
+            here_data.append(team_name)
+            data.append(here_data)
+        print(mininum + 1)
+        print(data)
+        print(sorted(data))
+        schedule = logic.create_Chart(data, (mininum+1))
+        print(schedule)
+        await ctx.send("Done")
+        db.close()    
+  
+    
 @client.command()
 async def removeteam(ctx, teamname):
     db = shelve.open('tournament.dat')
@@ -43,8 +86,6 @@ async def removeteam(ctx, teamname):
 
     await ctx.send("Removed!")
     db = shelve.close()
-
-#await channel.send(file=discord.File('my_image.png'))
 
 async def inputd(prompt, ctx):
     await ctx.send(prompt) # Sending the initial message
@@ -56,10 +97,34 @@ async def inputd(prompt, ctx):
     message = await client.wait_for("message", check=check)
     return message
 
-
-
-async def members(ctx, *, member):    
-    await ctx.send(member)
-    memberlist = []
-    await ctx.send("These are the members: " + member)
-client.run('ODczMzU3MzAyMzk5MzkzODIy.YQ3PXw.teLyegmNl9Niq3nvXuO8xH4Zk44')    
+@client.command()
+async def update(ctx):
+    m = await inputd("Which teams would you like to update", ctx)
+    teams = m.content
+    teams = teams.split()
+    print(teams)
+    db = shelve.open('tournament.dat')
+    # teamname W/L teamname2 W/L
+    changeteam1 = db[teams[0]] #team name 1
+    changeteam2 = db[teams[2]] #team name 2
+    win1 = changeteam1['wins'][0]
+    win2 = changeteam2['wins'][0]
+    
+    x = (win1 + int(teams[1]))
+    y = (win2 + int(teams[3]))    
+    changeteam1['wins'][0] = x
+    changeteam2['wins'][0] = y
+    
+    recordchange = changeteam1['record']
+    recordchange.append(teams[1])
+    changeteam1['record'] = recordchange
+    
+    recordchange2 = changeteam2['record']
+    recordchange2.append(teams[3])
+    changeteam2['record'] = recordchange2 
+    
+    db[teams[0]] = changeteam1
+    db[teams[2]] = changeteam2
+    
+    print(db[teams[0]])
+    print(db[teams[2]])
