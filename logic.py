e6e67df4e4ac5a49fa677d53cc2331df998ea10b
Zli123123 <67983161+Zli123123@users.noreply.github.com>
2021-08-07 11:20:02 -0400

    Add files via upload

--- logic.py
+++ logic.py
@@ -1,39 +1,34 @@
 import random
 round = 1
 
-
-data = [[2, "Sophie"], [-2, "Will"], [-2, "Khris"], [2, "Bob"], [1, "Sarah"]]
-
-#firstcheck
-members = 4
-if len(data) != members:
-    print("invalid - not corresponding with number of members")
-
-
-pairlist = []
-tournament = (sorted(data))
-if len(tournament) % 2 == 1:
-    pairlist.append(tournament[len(tournament) - 1])
-    tournament.pop()
-if round == 1:
-    random.shuffle(tournament)
+def creat_Chart(DATA, Round):
+    data = DATA
+    pairlist = []
+    tournament = (sorted(data))
+    if len(tournament) % 2 == 1:
+        pairlist.append(tournament[len(tournament) - 1])
+        tournament.pop()
+    if round == 1:
+        random.shuffle(tournament)
+        
+        for i in range(int(len(tournament)/2)):
+            pair = []
+            pair.append(tournament[i*2])
+            pair.append(tournament[(i*2)+1])
+            pairlist.append(pair)
+        print(pairlist) 
+        
+    if round != 1:
+        for i in range(int(len(tournament)/2)):
+            pair = []
+            pair.append(tournament[i*2])
+            pair.append(tournament[(i*2)+1])
+            pairlist.append(pair)
+        print(pairlist)
     
-    for i in range(int(len(tournament)/2)):
-        pair = []
-        pair.append(tournament[i*2])
-        pair.append(tournament[(i*2)+1])
-        pairlist.append(pair)
-    print(pairlist) 
-    
-if round != 1:
-    for i in range(int(len(tournament)/2)):
-        pair = []
-        pair.append(tournament[i*2])
-        pair.append(tournament[(i*2)+1])
-        pairlist.append(pair)
-    print(pairlist)
-            
-Pairs = open("Pairs", "w")
-for k in range(len(pairlist)):
-    Pairs.write(str(pairlist[k]))
-    Pairs.write("\n")
+    return pairlist
+    #Pairs = open("Pairs", "w")
+    #for k in range(len(pairlist)):
+        #Pairs.write(str(pairlist[k]))
+        #Pairs.write("\n")
+    #Pairs.close()
